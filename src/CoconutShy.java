ad18f26c53eba33a9436d56feae0baacfa3f77cc
Lasse Deleuran <lassedeleuran@gmail.com>
2022-04-02 01:46:49 +0200

    Updated Coconut Shy to use Track class, KillSwitch class, and new aux controller that detects shops.

--- src/CoconutShy.java
+++ src/CoconutShy.java
@@ -7,27 +7,24 @@ import lejos.nxt.*;
  * @author Lasse Deleuran
  */
 public class CoconutShy {
-	private static final NXTRegulatedMotor track = new NXTRegulatedMotor(MotorPort.A);
+	private static final Track track = new Track(MotorPort.A);
 	private static final NXTMotor gun = new NXTMotor(MotorPort.B);	
 	private static final UltrasonicSensor distance = new UltrasonicSensor(SensorPort.S1);
-	private static final AdjustablePowerOutput aux = new AdjustablePowerOutput(MotorPort.C, 3);
 
-	public static final int SPEED_TRACK = 100;
 	public static final int GUN_TURN = 8;
 	public static final int TRIGGER_DISTANCE = 3;
-	public static final int SHOTS_BEFORE_RECALIBRATION = 10;
 	
 	public static void main(String[] args) {
-		aux.start(); // Extra power output.
+		AuxController.setupAuxController(MotorPort.C, SensorPort.S2);
+		KillSwitch.enable();
 		
-		track.setSpeed(SPEED_TRACK);
-		track.forward();
+		track.out();
 		
 		resetGun(); // Ensure gun is ready to shoot
 		int lastDistance = distance.getDistance();
 		int shots = 0;
 		
-		while(!Button.ENTER.isDown()) {
+		while(true) {
 			Time.sleep(250);
 
 			int val = distance.getDistance();
@@ -47,16 +44,15 @@ public class CoconutShy {
 				shots = 0;
 			}
 		}
-		aux.stop();
 	}
 	
 	private static void reset() {
 		track.stop();
-		track.setSpeed(2*SPEED_TRACK);
+		track.boost();
 		track.rotate(-145);
 		track.rotate(180); // Ensure the lifters are down
-		track.setSpeed(SPEED_TRACK);
-		track.forward();
+		track.resetSpeed();
+		track.out();
 	}
 	
 	private static void shoot() {
