83a2999b080ba085cb05e6b2d8a57ea150ab8bb3
Lasse Deleuran <lasse.deleuran@lego.com>
2023-03-16 22:12:15 +0100

    Changed green to white as it reads more reliably vs gray that is seen as both blue and green.

--- src/BalloonWorld.py
+++ src/BalloonWorld.py
@@ -12,9 +12,31 @@ TURN_SPEED = 100
 TRACK_SPEED = 130
 TRACK2_SPEED = 3*TRACK_SPEED # Due to 1:3 gearing
 
-colors = [Color.BLUE, Color.BLUE, Color.BLUE, 
-          Color.RED, Color.YELLOW, Color.GREEN]
-colorIdx = 0 # First get 3 normal people inside (gray reads as blue)
+acceptedColors = [Color.RED, Color.YELLOW, Color.WHITE]
+colors = [Color.NONE, Color.NONE, Color.NONE]
+colorIdx = -3
+
+def acceptsColor(c):
+    global colorIdx, colors, acceptedColors
+    if colorIdx < 0: # Initially find 3 non-colored minifigs
+        print('Initially find 3 non-colored figures')
+        if c in acceptedColors:
+            return False
+        colorIdx += 1
+        return True
+    if colorIdx < 3: # Set up colors:
+        if c in colors or not c in acceptedColors:
+            return False
+        colors[colorIdx] = c
+        colorIdx += 1
+        return True
+    # Normal operation: Compare against saved colors:
+    expectedColor = colors[colorIdx % 3]
+    if c != expectedColor:
+        print('Normal operation. Expected', expectedColor)
+        return False
+    colorIdx += 1
+    return True
 
 turnTarget = 0
 def turn():
@@ -24,10 +46,10 @@ def turn():
 
 def go():
     track.run(TRACK_SPEED)
-    track2.run(36)
+    track2.run(35)
 
 def goAngle(angle):
-    track2.run(36)
+    track2.run(35)
     track.run_angle(TRACK_SPEED, angle)
     track2.stop()
 
@@ -39,13 +61,13 @@ def stop():
 def change():
     goAngle(220) # Get figure in front of entrance
 
-    track.run(40) # Ensure figure transition to track 2
-    track2.run_angle(TRACK2_SPEED, 1000) # Get figure inside
+    track.run(40) # Ensure figure transitions to track 2
+    track2.run_angle(TRACK2_SPEED, 1500) # Get figure inside
 
-    track.stop() # Prevent second figure from getting inside
-    track2.run_angle(TRACK2_SPEED, 2500) # Get figure fully inside
+    goAngle(3000) # Make other figs move past
+    track.run_angle(TRACK_SPEED, -500) # Make space outside
+    track2.run_angle(TRACK2_SPEED, 2000) # Get figure fully inside
 
-    track.run_angle(TRACK_SPEED, -200) # Make a bit more space outside
     track2.run_angle(TRACK2_SPEED, 1200, Stop.COAST, False) # Help the turner
     turn()
 
@@ -60,13 +82,14 @@ def clearSensor():
     print('Clearing sensor')
     go()
     sinceLast = 0
-    while sinceLast < 40:
+    while sinceLast < 30:
         sinceLast += 1
         wait(150)
         if sensor.color() != Color.NONE:
             sinceLast = 0
     print('Clean!')
 
+print()
 print('Console debugging enabled!')
 while True:
     wait(180)
@@ -74,12 +97,9 @@ while True:
     if color != Color.NONE:
         stop()
         goAngle(110) # Test again when center above sensor        
-        wait(100) # Stop to read color properly
+        wait(200) # Stop to read color properly
         color = sensor.color()
-        print('Looking for color',colors[colorIdx],'found',color)
-        if color is colors[colorIdx]:
-            colorIdx += 1
-            if colorIdx == 6:
-                colorIdx = 3
+        print('Found',color)
+        if acceptsColor(color):
             change()
         clearSensor()
