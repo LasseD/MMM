0ce8ac5b7f9e2bc6b6c57f07fb4808d50e2e0583
Lasse Deleuran <lasse.deleuran@lego.com>
2023-03-18 16:02:45 +0100

    CHange to version without backtracking of outer track.

--- src/BalloonWorld.py
+++ src/BalloonWorld.py
@@ -6,11 +6,11 @@ from pybricks.tools import wait
 hub = MoveHub()
 sensor = ColorDistanceSensor(Port.D)
 track = Motor(Port.C)
-track2 = Motor(Port.A)
+trackInside = Motor(Port.A)
 turner = Motor(Port.B)
 TURN_SPEED = 100
 TRACK_SPEED = 130
-TRACK2_SPEED = 3*TRACK_SPEED # Due to 1:3 gearing
+TRACK_INSIDE_SPEED = 3*TRACK_SPEED # Due to 1:3 gearing
 
 acceptedColors = [Color.RED, Color.YELLOW, Color.WHITE]
 colors = [Color.NONE, Color.NONE, Color.NONE]
@@ -41,58 +41,58 @@ def acceptsColor(c):
 turnTarget = 0
 def turn():
     global turnTarget
+    trackInside.run_angle(TRACK_INSIDE_SPEED, 1200, Stop.COAST, False) # Help the turner
     turnTarget -= 600 # 600 = 360*5/3/3*3
     turner.run_target(TURN_SPEED, turnTarget)
 
 def go():
     track.run(TRACK_SPEED)
-    track2.run(35)
+    trackInside.run(35)
 
 def goAngle(angle):
-    track2.run(35)
+    trackInside.run(35)
     track.run_angle(TRACK_SPEED, angle)
-    track2.stop()
+    trackInside.stop()
 
 def stop():
     track.stop()
-    track2.stop()
+    trackInside.stop()
+
+def clearSensor():
+    print('Clearing sensor')
+    go()
+    sinceLast = 0
+    while sinceLast < 48:
+        sinceLast += 1
+        wait(150)
+        if sensor.color() != Color.NONE:
+            sinceLast = 0
+    stop()
+    print('Clean!')
 
 # Change the figure. Assume the figure starts right over the color sensor.
 def change():
     goAngle(220) # Get figure in front of entrance
 
     track.run(40) # Ensure figure transitions to track 2
-    track2.run_angle(TRACK2_SPEED, 1500) # Get figure inside
+    trackInside.run_angle(TRACK_INSIDE_SPEED, 1500) # Get figure inside
 
-    goAngle(3000) # Make other figs move past
-    track.run_angle(TRACK_SPEED, -500) # Make space outside
-    track2.run_angle(TRACK2_SPEED, 2000) # Get figure fully inside
+    clearSensor()
+    trackInside.run_angle(TRACK_INSIDE_SPEED, 2000) # Get figure fully inside
 
-    track2.run_angle(TRACK2_SPEED, 1200, Stop.COAST, False) # Help the turner
     turn()
 
-    track2.run_angle(TRACK2_SPEED, -1500)
+    # Get figure from inside out
+    trackInside.run_angle(TRACK_INSIDE_SPEED, -1500)
     track.run(30)
-    track2.run_angle(TRACK2_SPEED, -2000)
+    trackInside.run_angle(TRACK_INSIDE_SPEED, -2000)
     go()
-
-go()
-
-def clearSensor():
-    print('Clearing sensor')
-    go()
-    sinceLast = 0
-    while sinceLast < 30:
-        sinceLast += 1
-        wait(150)
-        if sensor.color() != Color.NONE:
-            sinceLast = 0
-    print('Clean!')
 
 print()
 print('Console debugging enabled!')
 while True:
     wait(180)
+    go()
     color = sensor.color()
     if color != Color.NONE:
         stop()
